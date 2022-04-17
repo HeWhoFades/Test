13a234200b731820ce93960ae23266f6aaf2c73a
Panna Aryan <panna.codebase@gmail.com>
2022-04-17 14:13:07 +0600

    added a file

---
+++ no.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define ull unsigned long long
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout<<"LOL YOU HERE"<<endl;
+    
+    return 0;
+
+}
